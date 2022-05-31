ee1590b76a0459ea034744fd816b9cd5a693f0df
Fedesky25 <fedesky.guglielmi@gmail.com>
2022-05-31 11:12:27 +0200

    feat: navbar

--- public/js/index.js
+++ public/js/index.js
@@ -7,5 +7,11 @@
     see_more.addEventListener("click", e => {
         main.scrollIntoView({ behavior: "smooth" });
     });
+    const navbar = document.querySelector("nav");
+    const navbtn = navbar.querySelector(".burger");
+    navbtn.addEventListener("click", () => {
+        navbar.classList.toggle("open");
+        document.body.classList.toggle("no-scroll");
+    });
 
 })();
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,10 @@ const main = document.querySelector("main") as HTMLElement;
 see_more.addEventListener("click", e => {
     main.scrollIntoView({ behavior: "smooth" }); 
 });
+
+const navbar = document.querySelector("nav") as HTMLElement;
+const navbtn = navbar.querySelector(".burger") as HTMLButtonElement;
+navbtn.addEventListener("click", () => {
+    navbar.classList.toggle("open");
+    document.body.classList.toggle("no-scroll");
+});
