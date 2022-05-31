ce18449e99cca16cc062bf79c6f5a1b87144586e
Fedesky25 <fedesky.guglielmi@gmail.com>
2022-05-31 21:44:37 +0200

    feat: nav closes on touch out or back

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,59 @@
 (function () {
     'use strict';
 
+    function navSetup(identifier) {
+        const btn = document.querySelector(`[data-nav-btn="${identifier}"]`);
+        const nav = document.querySelector(`[data-nav="${identifier}"]`);
+        if (!nav || !btn)
+            return alert("No navbar with identifier " + identifier);
+        document.body.setAttribute("data-nav-open", "false");
+        const contents = document.querySelectorAll(`[data-nav-closer~="${identifier}"]`);
+        const OPEN_STATE = "open-state-" + identifier;
+        window.addEventListener("popstate", closeAction);
+        btn.addEventListener("click", toggle);
+        let isOpen = false;
+        function closeAction() {
+            if (!isOpen)
+                return;
+            isOpen = false;
+            nav.classList.remove("open");
+            btn.classList.remove("active");
+            document.body.classList.remove("has-nav-open");
+            for (var el of contents) {
+                el.classList.remove("has-nav-open");
+                el.removeEventListener("click", close);
+            }
+        }
+        function openAction() {
+            if (isOpen)
+                return;
+            isOpen = true;
+            nav.classList.add("open");
+            btn.classList.add("active");
+            document.body.classList.add("has-nav-open");
+            for (var el of contents) {
+                el.classList.add("has-nav-open");
+                el.addEventListener("click", close);
+            }
+        }
+        function close() { if (history.state === OPEN_STATE)
+            history.back(); }
+        function toggle() {
+            if (history.state === OPEN_STATE)
+                history.back();
+            else {
+                history.pushState(OPEN_STATE, "");
+                openAction();
+            }
+        }
+    }
+
     const see_more = document.getElementById("see-more");
     document.querySelector("header");
     const main = document.querySelector("main");
     see_more.addEventListener("click", e => {
         main.scrollIntoView({ behavior: "smooth" });
     });
-    const navbar = document.querySelector("nav");
-    const navbtn = navbar.querySelector(".burger");
-    navbtn.addEventListener("click", () => {
-        navbar.classList.toggle("open");
-        document.body.classList.toggle("no-scroll");
-    });
+    navSetup("main");
 
 })();
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { navSetup } from './utils';
+
 const see_more = document.getElementById("see-more") as HTMLButtonElement;
 const header = document.querySelector("header") as HTMLElement;
 const main = document.querySelector("main") as HTMLElement;
@@ -6,9 +8,5 @@ see_more.addEventListener("click", e => {
     main.scrollIntoView({ behavior: "smooth" }); 
 });
 
-const navbar = document.querySelector("nav") as HTMLElement;
-const navbtn = navbar.querySelector(".burger") as HTMLButtonElement;
-navbtn.addEventListener("click", () => {
-    navbar.classList.toggle("open");
-    document.body.classList.toggle("no-scroll");
-});
+
+navSetup("main");
