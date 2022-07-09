b91d00580ec45d6317e8ce833ce1aaa82b02c40e
Fedesky25 <fedesky.guglielmi@gmail.com>
2022-07-09 20:20:33 +0200

    projects page update + common styles + current page underlined

--- public/js/sponsors.js
+++ public/js/sponsors.js
@@ -8,6 +8,8 @@
             return alert("No navbar with identifier " + identifier);
         const closers = document.querySelectorAll(`[data-nav-closer~="${identifier}"]`);
         const OPEN_STATE = "open-state-" + identifier;
+        if (history.state === OPEN_STATE)
+            history.back();
         window.addEventListener("popstate", closeAction);
         btn.addEventListener("click", toggle);
         let isOpen = false;
@@ -93,5 +95,34 @@
 
     setupThemePreference();
     setupNavigation("main-nav", 80);
+    // const cards = document.querySelectorAll<HTMLElement>(".card");
+    // for(var card of cards) {
+    //     card.addEventListener("mouseenter", mouseenter);
+    //     card.addEventListener("mouseleave", mouseleave);
+    // }
+    // function smooth(el: HTMLElement) {
+    //     el.classList.add("smooth");
+    //     setTimeout(() => el.classList.remove("smooth"), 250);
+    // }
+    // function mouseenter(this: HTMLElement, ev: MouseEvent) {
+    //     smooth(this);
+    //     setmovement.call(this, ev);
+    //     this.addEventListener("mousemove", setmovement);
+    // }
+    // function mouseleave(this: HTMLElement, ev: MouseEvent) {
+    //     smooth(this);
+    //     this.removeEventListener("mousemove", setmovement);
+    //     this.style.setProperty("--x", "0");
+    //     this.style.setProperty("--y", "0");
+    // }
+    // function setmovement(this: HTMLElement, ev: MouseEvent) {
+    //     const h = this.clientHeight;
+    //     const w = this.clientWidth;
+    //     const x = ev.pageX - this.offsetLeft;
+    //     const y = ev.pageY - this.offsetTop;
+    //     // console.log(ev.offsetX, ev.offsetY, " in ", w, h);
+    //     this.style.setProperty("--x", (2*x/w - 1).toFixed(4));
+    //     this.style.setProperty("--y", (2*y/h - 1).toFixed(4));
+    // }
 
 })();
--- src/ts/sponsors.ts
+++ src/ts/sponsors.ts
@@ -2,3 +2,37 @@ import { setupThemePreference, setupNavigation } from './utils';
 
 setupThemePreference()
 setupNavigation("main-nav", 80);
+
+// const cards = document.querySelectorAll<HTMLElement>(".card");
+// for(var card of cards) {
+//     card.addEventListener("mouseenter", mouseenter);
+//     card.addEventListener("mouseleave", mouseleave);
+// }
+
+// function smooth(el: HTMLElement) {
+//     el.classList.add("smooth");
+//     setTimeout(() => el.classList.remove("smooth"), 250);
+// }
+
+// function mouseenter(this: HTMLElement, ev: MouseEvent) {
+//     smooth(this);
+//     setmovement.call(this, ev);
+//     this.addEventListener("mousemove", setmovement);
+// }
+
+// function mouseleave(this: HTMLElement, ev: MouseEvent) {
+//     smooth(this);
+//     this.removeEventListener("mousemove", setmovement);
+//     this.style.setProperty("--x", "0");
+//     this.style.setProperty("--y", "0");
+// }
+
+// function setmovement(this: HTMLElement, ev: MouseEvent) {
+//     const h = this.clientHeight;
+//     const w = this.clientWidth;
+//     const x = ev.pageX - this.offsetLeft;
+//     const y = ev.pageY - this.offsetTop;
+//     // console.log(ev.offsetX, ev.offsetY, " in ", w, h);
+//     this.style.setProperty("--x", (2*x/w - 1).toFixed(4));
+//     this.style.setProperty("--y", (2*y/h - 1).toFixed(4));
+// }
