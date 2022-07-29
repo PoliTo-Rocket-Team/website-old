24d7211d2bd10bf399789c026974812ce79489ce
Fabio Meloni <106022293+fabioski01@users.noreply.github.com>
2022-07-29 11:47:25 +0200

    feat: changed pie numbers and titles (test)

--- public/js/about.js
+++ public/js/about.js
@@ -147,23 +147,23 @@
 
     const pies = [
         {
-            title: "Members distribution among majors",
+            title: "Members by program",
             slices: [
-                { value: 16, label: "Aerospace", color: 0x3bdb84 },
-                { value: 1, label: "ECE", color: 0xdd4991 },
-                { value: 3, label: "Mechanical", color: 0xdd0011 },
-                { value: 1, label: "Physics", color: 0x2479cf },
-                { value: 1, label: "Other", color: 0xb28c23 },
+                { value: 16, label: "Aerospace Eng.", color: 0x3bdb84 },
+                { value: 1, label: "Electronic Eng.", color: 0xdd4991 },
+                { value: 3, label: "MechanicalEng.", color: 0xdd0011 },
+                { value: 1, label: "Physics Eng.", color: 0x2479cf },
+                { value: 1, label: "Other non-Eng.", color: 0xb28c23 },
             ],
             rotate: 255,
             threshold: 5,
         },
         {
-            title: "Members' degrees",
+            title: "Members by level",
             slices: [
-                { value: 11, label: "Bachelor", color: 0x4e7bc1 },
-                { value: 9, label: "Master", color: 0xe1a463 },
-                { value: 1, label: "Phd", color: 0x3bdb84 },
+                { value: 11, label: "Bachelor's", color: 0x4e7bc1 },
+                { value: 9, label: "Master's", color: 0xe1a463 },
+                { value: 1, label: "Ph.D.", color: 0x3bdb84 },
             ],
             rotate: 0,
             threshold: 0,
