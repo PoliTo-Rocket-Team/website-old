f3826b92ff79af5f3d2b1a454116a66b5f674a53
Fabio Meloni <106022293+fabioski01@users.noreply.github.com>
2022-07-29 11:44:21 +0200

    feat: changed mech eng. number in pie

--- public/js/about.js
+++ public/js/about.js
@@ -151,9 +151,9 @@
             slices: [
                 { value: 16, label: "Aerospace", color: 0x3bdb84 },
                 { value: 1, label: "ECE", color: 0xdd4991 },
-                { value: 1, label: "Mechanical", color: 0xdd0011 },
+                { value: 3, label: "Mechanical", color: 0xdd0011 },
                 { value: 1, label: "Physics", color: 0x2479cf },
-                { value: 3, label: "Other", color: 0xb28c23 },
+                { value: 1, label: "Other", color: 0xb28c23 },
             ],
             rotate: 255,
             threshold: 5,
