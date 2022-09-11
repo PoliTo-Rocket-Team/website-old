5721f6273d37e4e3652eabe507ffc46f97b180b1
Fedesky25 <fedesky.guglielmi@gmail.com>
2022-09-11 19:02:43 +0200

    chore: render output in public

--- render.js
+++ render.js
@@ -2,6 +2,6 @@ const { compile } = require("./src/build/html-components");
 const { writeFile } = require("fs/promises");
 
 process.argv.slice(2).forEach(name => compile(`./src/html/${name}.html`).then(res => {
-    if(res) writeFile(`./public/prova/${name}.html`, res, "utf-8").then(() => console.log(` > ${name} rendered`))
+    if(res) writeFile(`./public/${name}.html`, res, "utf-8").then(() => console.log(` > ${name} rendered`))
     else console.log(` > ${name} could not be rendered`);
 }))
