3a398cf74aa062adf510f24f3f4cd2d843da994e
Fabio Meloni <106022293+fabioski01@users.noreply.github.com>
2022-09-20 10:41:50 +0200

    [text] updated pies sarcletti

--- public/js/about.js
+++ public/js/about.js
@@ -149,10 +149,10 @@
         {
             title: "Members by program",
             slices: [
-                { value: 17, label: "Aerospace Engineering", color: "#3bdb84" },
+                { value: 16, label: "Aerospace Engineering", color: "#3bdb84" },
                 { value: 1, label: "Electronic Engineering", color: "#dd4991" },
                 { value: 3, label: "Mechanical Engineering", color: "#dd0011" },
-                { value: 1, label: "Physics Engineering", color: "#2479cf" },
+                { value: 1, label: "Physical Engineering", color: "#2479cf" },
                 { value: 1, label: "Other non-Engineering", color: "#e1a463" },
             ],
             rotate: 255,
@@ -161,8 +161,8 @@
         {
             title: "Members by level",
             slices: [
-                { value: 11, label: "Bachelor's", color: "#4e7bc1" },
-                { value: 9, label: "Master's", color: "#e1a463" },
+                { value: 12, label: "Bachelor's", color: "#4e7bc1" },
+                { value: 7, label: "Master's", color: "#e1a463" },
                 { value: 2, label: "Ph.D.", color: "#3bdb84" },
             ],
             rotate: 0,
@@ -171,7 +171,7 @@
         {
             title: "International students rate",
             slices: [
-                { value: 20, label: "Domestic", color: "#dd4991" },
+                { value: 19, label: "Domestic", color: "#dd4991" },
                 { value: 2, label: "International", color: "#e1a463" },
             ],
             rotate: 0,
